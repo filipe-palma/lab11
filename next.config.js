576170c4a150176bf54bb1a806aeac3169c5febb
Filipe Alexandre Almeida da Palma <a22402827@alunos.ulht.pt>
2024-12-10 03:18:38 +0000

    Refactor Next.js configuration: migrate from TypeScript to JavaScript and update tsconfig to include next.config.js

---
+++ next.config.js
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    domains: ['deisishop.pythonanywhere.com'],
+  },
+  // other config options here
+};
+
+module.exports = nextConfig;

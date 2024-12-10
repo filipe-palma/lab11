15b17cb87d45fd69fbdfbdd8d8d77c8ebe29c11d
Filipe Alexandre Almeida da Palma <a22402827@alunos.ulht.pt>
2024-12-10 03:59:12 +0000

    Migrate Next.js configuration from JavaScript to TypeScript; update dependencies to Next.js 15.0.3 and React 19.0.0-rc; adjust tsconfig include paths

--- next.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  images: {
-    domains: ['deisishop.pythonanywhere.com'],
-  },
-  // other config options here
-};
-
-module.exports = nextConfig;
---
+++ next.config.ts
@@ -0,0 +1,7 @@
+import type { NextConfig } from "next";
+const nextConfig: NextConfig = {
+  images: { 
+    domains: ['deisishop.pythonanywhere.com'], 
+  }, 
+};
+export default nextConfig;
